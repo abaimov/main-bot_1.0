155c4e7caf9c9ce3260fdf7cb30b94ce20def228
rickmoralis <megapowerbob@gmail.com>
2024-08-20 22:48:51 +0300

    fix err

--- app.mjs
+++ app.mjs
@@ -1,8 +1,7 @@
 import {Bot, GrammyError, HttpError,InlineKeyboard} from "grammy";
-// import {PrismaClient} from '@prisma/client';
 import dotenv from 'dotenv';
 dotenv.config();
-const prisma = new PrismaClient()
+
 
 const bot = new Bot(process.env.TOKEN);
 

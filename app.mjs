32a0f00c25cf2bb20a4997c2d11b5105ec03b3c1
rickmoralis <megapowerbob@gmail.com>
2024-08-20 23:27:27 +0300

    fix if

--- app.mjs
+++ app.mjs
@@ -1,16 +1,17 @@
-import {Bot, GrammyError, HttpError, InlineKeyboard} from "grammy";
-import {PrismaClient} from '@prisma/client';
+import { Bot, GrammyError, HttpError, InlineKeyboard } from "grammy";
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const bot = new Bot(process.env.TOKEN_SECOND);
+const bot = new Bot(process.env.TOKEN);
 
 export const texts = {
     MAIN_POST: `*Этот бот – полная замена официального сайта 1win в России и странах СНГ\\.*\n\nМы вывели казино на новый уровень: \nтеперь можно играть в любимые слоты прямо в Telegram 🎰\n\n||Нажимайте на кнопку "Регистрация" и получите бонус \\+500% к депозиту и 30% кэшбэк на казино 💸||`,
     WHAT_CAN_DO: `*Что умеет этот бот\\?*\n\n1️⃣\\. Полная замена официального сайта 1win\\.\n2️⃣\\. Удобная регистрация и вход через Telegram\\.\n3️⃣\\. Играйте в любимые слоты прямо в мессенджере\\! 🎰\n`
 };
+
 export const keyboards = {
     MAIN_KEYBOARD: new InlineKeyboard()
         .webApp("💎Регистрация", "https://1win-global-pro.com/officialbot")
@@ -23,29 +24,26 @@ export const keyboards = {
 
     BACK_KEYBOARD: new InlineKeyboard().text("⬅️ НАЗАД", "back")
 };
+
 export const images = {
     MAIN_IMAGE: "https://lh3.googleusercontent.com/fife/ALs6j_HuffTPMSOXwHDBaQd50tXlIFuuOjsfOLHiyvF9vp6j41oRihblZlbH_KgWgJYiXyFZlx4gZMB-1esuonlkWtabRct0mEDQxMwIjjQzvcC5ypsdEZBzc8wpO7ohwuXFvi5PwQvKy7z9RcIjBRAfzOee99qei3inCAibLL3R7pTSg-BV4O7RX8w-BtvPrEYxsBc5UT0_MuRx0oxzsRdOiPeyp8kAPH7uj5sUY5dxRuYidlmqZ-Mu5w_k38Y-J7pHmey6aAyNL7iHIlTlix2J25t3F1zxbtEDH241RebwQ4N0TK3rw8UXg-IIol6dC2YKsEkZzGY_KaWQbMtspjBKiokgh4RmWpzR0kdD4rNgweoI1f4fCof8ZRlg1V1Ol5cZFPFQBrF515Iff29ievDzYJCCYdxGvrWQS5k7MbkuSGA8sv7xawu2ZFslkSbNJua4iGT4keHP79kMgaDVndb5JCV5uw7n66DVd8oRaWoic48qewVGLhsGUDziNbptrVLUFYqmvIWuJRpy1FW01c7o8aAu-0_VWjJhaL5xzdvbpunDxsQyMVvtEkyY1LDnqesrelCITG5tB1sm1OReEia6UO0aCYjLhZ90HySRo_pRgPCegdPRYVfC_1y_a8z1NhzCGl3nxDWNs7nhRKMpcX8WCjDjaFw_mKujBW__mSxDzrVuzyOvU8iLgVgqkpTT2CnLKOGig0au_hMk9JKWiD3Cj6EjjIBgF288eReRBPcJycDQZXgvxwJnwtqJfPU96XESQGc_-RbTBTUDsHTYYHNwFEXoxAISwSSfULrQ7pegot4zKhB5uRkw5ZaIYN6_UqqQSTmHPqkM6FE8YFD9jDWZ35ycliJ3w0YWtTu_z83BVPOJtrtAUIwnLulIv7CFUL3KOFkTKsZmOiLDwH7Xcz2wtyGtJPKKGjwiMBcuD7vHcN0mtRuSDwoNmBfp7UrbNuV4q2yEhx_PBXsD48o9UY2z8L7t6aIoKj49kJhwdid9CNJJVq3uEh_cK_GLL4i1fpPoqFE1yYLH9ESxpsQiKyq-tyI4R87kFtvoldDkFBGOqQePOY11TkeDaz2oymwSgcHjRzX2OgBmRIPdQVt2RjEt1BI2-OsQTHqqX3wC7O-TodEfqr42IsuzfBsjwTg6A_ECYwYJJKYDg8sbw9sSHdVsQe8c3vYUR616FPpYlvuVd6xCFbU7uwbw6hJf2b6wT20POdmX8GgYp1dskC_RrCF_IFGRedQQbUcw6HxBpE3VLIj4teAp2cdBay3cI9kqwN_6LSKfgIWLql9CSgSAJss_ubHjUBd-aBbfyxoPqAoHZ8AMqkw7kfqINZxEHlpYXYbTVVfjrcjEH5O0MFrf3XWW_rvyuK5mnSONKGRywp4F14iqb8TnWrgezfN0-7I6L1XtvO74GsZaQBQNSevRwcPq_krOD5_PCDthO8mDaN3t8W4un3T2Bi2mXNbUK1xBtTDycyZQ6COMxIy6TnEVIR8zhMwTwV1hNk9hhDiLnTS_w4N2hxMKF1CuJNPkFqp4RqGiIGvz8u3dnGEbHfB09HkTCTRSJKK2-LnjEfGk1piUjN1nLtMX3mykaKVQC0RneAYlA_nP_-WKiI8Fel_ADrmcfFpPGGcUz72w3LzqhEyTjS4RDX2CiuJArFfWCPgJ5-heppkP_5UptN2L13SAxmRU2HMYSDI=w2760-h1508"
 }
 
-
 bot.on('message', async (ctx) => {
     const TEXT = ctx.message.text;
-    const LOCATION = ctx.from.language_code;
+    const LOCATION = ctx.from.language_code || "unknown";  // Защита от undefined
+
     if (TEXT === "/start") {
         if (LOCATION === "ru") {
             try {
                 await prisma.user.create({
                     data: {
                         telegramId: String(ctx.from.id),
-                        language: ctx.from.language_code,
+                        language: LOCATION,
                         nickname: ctx.from.username
                     }
                 });
-            } catch (e) {
-                console.log(`Не удалось создать пользователя ID: ${ctx.from.id}, LANG: ${ctx.from.language_code}`);
-            }
 
-            try {
                 await ctx.react("👍");
                 const answer = await ctx.replyWithPhoto(images.MAIN_IMAGE, {
                     caption: texts.MAIN_POST,
@@ -75,16 +73,15 @@ bot.on('message', async (ctx) => {
             try {
                 await prisma.blocked.create({
                     data: {
-                        telegramId: ctx.from.id.toString(),
-                        language: ctx.from.language_code,
+                        telegramId: String(ctx.from.id),
+                        language: LOCATION,
                         nickname: ctx.from.username
                     }
                 });
-                return null;
-            } catch (e) {
-                console.log(`Не удалось заблокировать пользователя ID: ${ctx.from.id}, LANG: ${ctx.from.language_code}`);
+            } catch (error) {
+                console.log(`Не удалось заблокировать пользователя ID: ${ctx.from.id}, LANG: ${LOCATION}`);
             }
-
+            return null;
         }
     }
 });
